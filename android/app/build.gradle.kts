eb8b67de7e20d3fc82912d4cfd388a5b5f98d548
Bureau mobile-platform <mobile-platform@bureau.local>
2026-08-01 08:10:51 +0000

    chore: android config (applicationId, minSdk/compileSdk, biometric activity, AppCompat theme, permissions)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "dev.jcqb.cash_me_outside"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -15,11 +15,10 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "dev.jcqb.cash_me_outside"
+        applicationId = "dev.jcqb.cashmeoutside"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- android/app/src/main/kotlin/dev/jcqb/cash_me_outside/MainActivity.kt
+++ android/app/src/main/kotlin/dev/jcqb/cash_me_outside/MainActivity.kt
@@ -1,5 +1,5 @@
 package dev.jcqb.cash_me_outside
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
